06fc764d4105a52f6753dab5ceed3f93925846ae
phikhanhcr <sykhanhsky@gmail.com>
2022-09-30 01:26:40 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-<script>
-  // initial time
-  const maxDay = 7;
-  document.getElementById("day-base-timer-label").textContent = maxDay;
-  const PIR2 = 283;
+// initial time
+const maxDay = 7;
+document.getElementById("day-base-timer-label").textContent = maxDay;
+const PIR2 = 283;
 
-  function init(day) {
-    const maxDay = day;
+
+init(maxDay)
+function init(day) {
+  const maxDay = day;
   const maxSecond = 60;
   const maxMin = 60;
   const maxHour = 24;
@@ -21,33 +22,16 @@
 
   const timeType = {
     second: "second",
-  min: "min",
-  hour: "hour",
-  day: "day",
-    };
+    min: "min",
+    hour: "hour",
+    day: "day",
+  };
 
   startTimer();
   let checkFirstTime = true;
-  function onFirstTime() {
-    document.getElementById("day-base-timer-label").textContent =
-    maxDay - 1;
-  document.getElementById("hour-base-timer-label").textContent =
-  maxHour - 1;
-  document.getElementById("min-base-timer-label").textContent =
-  maxMin - 1;
-  timeLeftSecond = maxSecond - 1;
-  timeLeftMin = maxMin - 1;
-  timeLeftHour = maxHour - 1;
-  timeLeftDay = maxDay - 1;
-  setCircleDasharray(timeLeftSecond, timeType.second, maxSecond);
-  setCircleDasharray(timeLeftMin, timeType.min, maxMin);
-  setCircleDasharray(timeLeftHour, timeType.hour, maxHour);
-  setCircleDasharray(timeLeftDay, timeType.day, maxDay);
-    }
   function startTimer() {
     timerInterval = setInterval(() => {
       if (checkFirstTime) {
-        console.log("123");
         onFirstTime();
         checkFirstTime = false;
       }
@@ -93,68 +77,62 @@
         resetTime(timeType.second);
       }
     }, 1000);
-    }
+  }
 
   function resetTime(type) {
-      switch (type) {
-        case timeType.second:
-  timePassedSecond = 0;
-  timeLeftSecond = maxSecond;
-  break;
-  case timeType.min:
-  timePassedMin = 0;
-  timeLeftMin = maxSecond;
-  break;
-  case timeType.hour:
-  timePassedHour = 0;
-  timeLeftHour = maxHour;
-  break;
-  case timeType.day:
-  break;
-      }
+    switch (type) {
+      case timeType.second:
+        timePassedSecond = 0;
+        timeLeftSecond = maxSecond;
+        break;
+      case timeType.min:
+        timePassedMin = 0;
+        timeLeftMin = maxSecond;
+        break;
+      case timeType.hour:
+        timePassedHour = 0;
+        timeLeftHour = maxHour;
+        break;
+      case timeType.day:
+        break;
     }
   }
+  function onFirstTime() {
+    document.getElementById("day-base-timer-label").textContent =
+      maxDay - 1;
+    document.getElementById("hour-base-timer-label").textContent =
+      maxHour - 1;
+    document.getElementById("min-base-timer-label").textContent =
+      maxMin - 1;
+    timeLeftSecond = maxSecond - 1;
+    timeLeftMin = maxMin - 1;
+    timeLeftHour = maxHour - 1;
+    timeLeftDay = maxDay - 1;
+    setCircleDasharray(timeLeftSecond, timeType.second, maxSecond);
+    setCircleDasharray(timeLeftMin, timeType.min, maxMin);
+    setCircleDasharray(timeLeftHour, timeType.hour, maxHour);
+    setCircleDasharray(timeLeftDay, timeType.day, maxDay);
+  }
+}
 
-  function formatTime(time) {
-    if (time < 10) {
+
+function formatTime(time) {
+  if (time < 10) {
     time = `0${time}`;
-    }
+  }
   return `${time}`;
-  }
+}
 
-  function calculateTimeFraction(timeLeft, maxValue) {
-    const rawTimeFraction = timeLeft / maxValue;
+function calculateTimeFraction(timeLeft, maxValue) {
+  const rawTimeFraction = timeLeft / maxValue;
   return rawTimeFraction - (1 / maxValue) * (1 - rawTimeFraction);
-  }
+}
 
-  function setCircleDasharray(timeLeft, type, maxValue) {
-    const circleDasharray = `${(
+function setCircleDasharray(timeLeft, type, maxValue) {
+  const circleDasharray = `${(
     calculateTimeFraction(timeLeft, maxValue) * PIR2
   ).toFixed(0)} 283`;
-  console.log({circleDasharray});
   document
-  .getElementById(`${type}-base-timer-path-remaining`)
-  .setAttribute("stroke-dasharray", circleDasharray);
-  }
-
-    // function setUI(time, type) {
-    //   if (time == 0) {
-    //     switch (type) {
-    //       case timeType.second:
-    //         timePassedMin = timePassedMin += 1;
-    //         timeLeftMin = maxMin - timePassedMin;
-    //         document.getElementById(
-    //           `${timeType.min}-base-timer-label`
-    //         ).innerHTML = formatTime(timeLeftMin);
-    //         setCircleDasharray(timeLeftMin, timeType.min, maxMin);
-    //         if (timeLeftMin === 0) {
-    //           setUI(timeLeftMin);
-    //         }
-    //         break;
-    //       case timeType.min:
-    //       case timeType.hour:
-    //       case timeType.day:
-    //     }
-    //   }
-    // }
-</script>
+    .getElementById(`${type}-base-timer-path-remaining`)
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
